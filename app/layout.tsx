ae5a1020835c4ea034a96ba78d0bd6a9138790f3
SPR1NGG <82774613+SPR1NGG@users.noreply.github.com>
2023-03-20 14:37:14 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
+'use client';
+import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import Providers from './Provider';
-export const metadata = {
-	title: 'Aresmeta',
-	description: 'Сайт для создания конференций',
-};
+// export const metadata = {
+// 	title: 'Aresmeta',
+// 	description: 'Сайт для создания конференций',
+// };
 
 interface IProps {
 	children: React.ReactNode;
 	session: any;
 }
 
-export default function RootLayout({ children }: IProps) {
+export default function RootLayout({ children, session }: IProps) {
 	return (
 		<html lang="en">
 			<body className="bg-[#F2F2ED]">
-				<Providers>{children}</Providers>
+				<SessionProvider session={session}> {children}</SessionProvider>
 			</body>
 		</html>
 	);
